c0161690f275e4dcd274e2e95f86d42afd59d870
TusharSachde <tushar@wohlig.com>
2016-02-27 16:20:13 +0530

    Fancy box

--- js/app.js
+++ js/app.js
@@ -39,3 +39,28 @@ firstapp.directive('img', function($compile, $parse) {
     }
   };
 });
+
+firstapp.directive('fancyboxBox', function($document) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function(scope, element, attr) {
+            var $element = $(element);
+            var target;
+            if (attr.rel) {
+               target = $("[rel='" + attr.rel + "']");
+            } else {
+                target = element;
+            }
+
+            target.fancybox({
+                openEffect: 'fade',
+                closeEffect: 'fade',
+                closeBtn: true,
+                helpers: {
+                    media: {}
+                }
+            });
+        }
+    };
+});
